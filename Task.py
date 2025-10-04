3dcc29033801f00172e73256ed11f6693887bea3
ssglob <edwardlaw21@hotmail.com>
2025-10-04 13:42:15 -0700

    Finished Task.py

--- Task.py
+++ Task.py
@@ -1,8 +1,16 @@
 class Task:
-    def __init__(self, task, date):
+    def __init__(self, task, date, duration):
         self.task = task
+        self.date = date
+        self.duration = duration        
 
     def update(self, task, dictionary):
-        pass
+        if dictionary["task"]:
+            self.task = task.task
+        
+        if dictionary["date"]:
+            self.date = task.date
 
+        if dictionary["duration"]:
+            self.duration = task.duration
     
