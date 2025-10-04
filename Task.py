9cd5592c3712da5c2b545c2ab74f2027b921d935
ssglob <edwardlaw21@hotmail.com>
2025-10-04 14:11:13 -0700

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,16 +1,6 @@
 class Task:
     def __init__(self, task, date, duration):
-        self.task = task
-        self.date = date
-        self.duration = duration        
+        self.task_data = {"task": task, "date": date, "duration": duration}
 
     def update(self, dictionary):
-        if dictionary["task"]:
-            self.task = dictionary["task"]
-        
-        if dictionary["date"]:
-            self.date = dictionary["date"]
-
-        if dictionary["duration"]:
-            self.duration = dictionary["duration"]
-    
+        self.task_data = dictionary
